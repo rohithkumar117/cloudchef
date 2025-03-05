8bfd0c8fb58856d877ece5e1c63dc422d83ac86a
Katam Srinith Reddy <145486953+Srinith2224@users.noreply.github.com>
2025-03-06 03:38:56 +0530

    Revert "Hide .env file and remove from history"

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
